72c79fc87332afc2f051f9964ee374ca5533ae66
Milos Levacic <milos@levacic.net>
2014-02-16 18:53:08 +0100

    Refactored and documented all tests

--- tests/BreadcrumbsTest.php
+++ tests/BreadcrumbsTest.php
@@ -5,7 +5,7 @@ use Way\Tests\Should;
 
 class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 
-	public function crumbProvider()
+	public function crumbsProvider()
 	{
 		return array(
 			array(
@@ -36,18 +36,46 @@ class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 					),
 				),
 			),
+			array(
+				array(
+					array(
+						'name' => 'Admin home',
+						'href' => '/admin',
+					),
+					array(
+						'name' => 'Stores',
+						'href' => 'stores',
+					),
+					array(
+						'name' => 'Store Foo',
+						'href' => 'http://website.com/admin/stores/store-foo',
+					),
+					array(
+						'name' => 'Secure product creation',
+						'href' => 'https://website.com/admin/stores/store-foo/add-product',
+					),
+				),
+			),
 		);
 	}
 
-	public function classProvider()
+	public function invalidCrumbsProvider()
 	{
 		return array(
-			array('bcrumb'),
-			array('bread'),
+			array(
+				array(
+					array(),
+					'test',
+					array(
+						'name' => 123,
+						'href' => new stdClass,
+					),
+				),
+			),
 		);
 	}
 
-	public function fullCrumbProvider()
+	public function crumbsWithCssClassesProvider()
 	{
 		return array(
 			array(
@@ -62,20 +90,115 @@ class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 					),
 					array(
 						'name' => 'Store Foo',
-						'href' => 'http://vsc.localhost/admin/stores/store-foo',
+						'href' => 'http://website.com/admin/stores/store-foo',
 					),
 				),
 				array(
-					'bcrumb',
+					'breadcrumbs-class',
+				),
+			),
+			array(
+				array(
+					array(
+						'name' => 'Admin home',
+						'href' => '/admin',
+					),
+					array(
+						'name' => 'Stores',
+						'href' => 'stores',
+					),
+					array(
+						'name' => 'Store Foo',
+						'href' => 'http://website.com/admin/stores/store-foo',
+					),
+				),
+				array(
+					'breadcrumbs-class',
+					'additional-breadcrumbs-class',
 				),
 			),
 		);
 	}
 
+	public function cssClassesProvider()
+	{
+		return array(
+			array('breadcrumbs-class'),
+			array('crumbs'),
+		);
+	}
+
 	/**
-	 * @dataProvider crumbProvider
+	 * Tests whether `Breadcrumbs::isValidCrumb()` provides proper validation
+	 * for valid crumbs.
+	 *
+	 * @dataProvider crumbsProvider
 	 */
-	public function testAddBreadcrumb($crumbs)
+	public function testIsValidCrumb($crumbs)
+	{
+		foreach ($crumbs as $key => $validCrumb)
+		{
+			Assert::true(Breadcrumbs::isValidCrumb($validCrumb));
+		}
+
+		$invalidCrumb = array();
+
+		Assert::false(Breadcrumbs::isValidCrumb($invalidCrumb));
+	}
+
+	/**
+	 * Tests whether `Breadcrumbs::isValidCrumb()` provides proper validation
+	 * for invalid crumbs.
+	 *
+	 * @dataProvider invalidCrumbsProvider
+	 */
+	public function testIsNotValidCrumb($crumbs)
+	{
+		foreach ($crumbs as $key => $invalidCrumb)
+		{
+			Assert::false(Breadcrumbs::isValidCrumb($invalidCrumb));
+		}
+	}
+
+	/**
+	 * @dataProvider cssClassesProvider
+	 */
+	public function testCssClasses($classes)
+	{
+		$b = new Breadcrumbs(array(), $classes);
+		Assert::count(count($classes), $b->getBreadcrumbsCssClasses());
+		$b->removeCssClasses($classes);
+		Assert::count(0, $b->getBreadcrumbsCssClasses());
+		$b->addCssClasses($classes);
+		Assert::count(count($classes), $b->getBreadcrumbsCssClasses());
+	}
+
+	/**
+	 * Tests whether `Breadcrumbs::setDivider()` works.
+	 */
+	public function testSetDivider()
+	{
+		$b = new Breadcrumbs();
+
+		$b->setDivider('@');
+
+		$b->addCrumb('foo', 'bar');
+		$b->addCrumb('foo', 'bar');
+
+		$crawler = new Symfony\Component\DomCrawler\Crawler($b->render());
+
+		$dividerText = $crawler->filter('span.divider')->first()->text();
+
+		Assert::same('@', $dividerText);
+	}
+
+	/**
+	 * Tests whether adding breadcrumbs one by one works, using
+	 * `Breadcrumbs::addCrumb()`.
+	 *
+	 * @dataProvider crumbsProvider
+	 */
+	public function testAddCrumb($crumbs)
 	{
 		$b = new Breadcrumbs;
 
@@ -88,9 +211,12 @@ class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * @dataProvider crumbProvider
+	 * Tests whether setting an array of breadcrumbs at once works, using
+	 * `Breadcrumbs::setBreadcrumbs()`.
+	 *
+	 * @dataProvider crumbsProvider
 	 */
-	public function testSetBreadcrumb($crumbs)
+	public function testSetBreadcrumbs($crumbs)
 	{
 		$b = new Breadcrumbs;
 
@@ -100,90 +226,138 @@ class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * @dataProvider crumbProvider
+	 * Tests whether `Breadcrumbs::setBreadcrumbs()` throws the correct exception
+	 * when called with invalid arguments.
+	 *
+	 * @dataProvider invalidCrumbsProvider
+	 * @expectedException InvalidArgumentException
 	 */
-	public function testValidCrumb($crumbs)
+	public function testSetBreadcrumbsException($crumbs)
 	{
-		foreach($crumbs as $key => $value)
-		{
-			Assert::true(Breadcrumbs::isValidCrumb($value));
-		}
+		$b = new Breadcrumbs;
 
-		$falseCrumb = array();
-		Assert::false(Breadcrumbs::isValidCrumb($falseCrumb));
+		$b->setBreadcrumbs($crumbs);
 	}
 
 	/**
-	 * @dataProvider classProvider
-	 */
-	public function testCssClasses($classes)
-	{
-		$b = new Breadcrumbs(array(), $classes);
-		Assert::count(count($classes), $b->getBreadcrumbsCssClasses());
-		$b->removeCssClasses($classes);
-		Assert::count(0, $b->getBreadcrumbsCssClasses());
-		$b->addCssClasses($classes);
-		Assert::count(count($classes), $b->getBreadcrumbsCssClasses());
-	}
-
-	public function testDivider()
-	{
-		$b = new Breadcrumbs();
-		$b->setDivider("@");
-		Assert::same("@", $b->getDivider());
-	}
-
-	/**
-	 * @dataProvider fullCrumbProvider
+	 * Tests whether the output is correctly rendered.
+	 *
+	 * Doesn't check the actual items, just that the expected number of items is
+	 * present.
+	 *
+	 * @dataProvider crumbsWithCssClassesProvider
 	 */
 	public function testOutput($crumbs, $classes)
 	{
 		$b = new Breadcrumbs($crumbs, $classes);
+
 		$crawler = new Symfony\Component\DomCrawler\Crawler($b->render());
-		Assert::count(1, $crawler->filter('ul.'.$classes[0]));
+
+		/**
+		 * There should only be one `ul` element.
+		 */
+		Assert::count(1, $crawler->filter('ul'));
+
+		/**
+		 * There should be as many `li` elements as there are breadcrumbs in the
+		 * original data.
+		 */
 		Assert::count(count($crumbs), $crawler->filter('li'));
+
+		/**
+		 * There should be one `span.divider` less than there are breadcrumbs.
+		 */
 		Assert::count(count($crumbs)-1, $crawler->filter('span.divider'));
 	}
 
 	/**
-	 * @dataProvider fullCrumbProvider
+	 * Tests that the output has the correct CSS classes applied.
+	 *
+	 * @dataProvider crumbsWithCssClassesProvider
+	 */
+	public function testOutputCssClasses($crumbs, $classes)
+	{
+		$b = new Breadcrumbs($crumbs, $classes);
+
+		$crawler = new Symfony\Component\DomCrawler\Crawler($b->render());
+
+		$normalizedExpectedClasses = $classes;
+		sort($normalizedExpectedClasses);
+
+		$ulClasses = $crawler->filter('ul')->first()->attr('class');
+		$normalizedUlClasses = explode(' ', trim($ulClasses));
+		sort($normalizedUlClasses);
+
+		Assert::same($normalizedExpectedClasses, $normalizedUlClasses);
+	}
+
+	/**
+	 * Tests that no dividers are rendered if the divider is set to `null`.
+	 *
+	 * @dataProvider crumbsWithCssClassesProvider
 	 */
 	public function testOutputWithoutDividers($crumbs, $classes)
 	{
 		$b = new Breadcrumbs($crumbs, $classes);
+
 		$b->setDivider(null);
 
 		$crawler = new Symfony\Component\DomCrawler\Crawler($b->render());
+
 		Assert::count(1, $crawler->filter('ul.'.$classes[0]));
 		Assert::count(count($crumbs), $crawler->filter('li'));
 		Assert::count(0, $crawler->filter('span.divider'));
 	}
 
 	/**
-	 * @dataProvider fullCrumbProvider
+	 * Tests whether full URLs are recognized correctly.
+	 *
+	 * @dataProvider crumbsProvider
 	 */
-	public function testHttpLinks($crumbs, $classes)
+	public function testFullUrls($crumbs)
 	{
-		$b = new Breadcrumbs($crumbs, $classes);
-		$i = 0;
-		$crumbs = $b->getBreadcrumbs();
-		foreach ($crumbs as $crumb)
+		$b = new Breadcrumbs($crumbs);
+
+		foreach ($b->getBreadcrumbs() as $key => $crumb)
 		{
-			Assert::equals($crumb['href'], $crumbs[$i]['href']);
-			$i++;
+			$originalCrumb = $crumbs[$key];
+
+			$hrefIsFullUrl = $crumb['hrefIsFullUrl'];
+
+			if (mb_substr($originalCrumb['href'], 0, 7) === 'http://')
+			{
+				Assert::true($hrefIsFullUrl);
+			}
+			else if (mb_substr($originalCrumb['href'], 0, 8) === 'https://')
+			{
+				Assert::true($hrefIsFullUrl);
+			}
+			else if (mb_substr($originalCrumb['href'], 0, 1) === '/')
+			{
+				Assert::true($hrefIsFullUrl);
+			}
+			else
+			{
+				Assert::false($hrefIsFullUrl);
+			}
 		}
 	}
 
-	public function testCountBreadcrumbs()
+	/**
+	 * Tests whether `Breadcrumbs::count()` works correctly.
+	 *
+	 * @dataProvider crumbsProvider
+	 */
+	public function testCountBreadcrumbs($crumbs)
 	{
-		$b = new Breadcrumbs();
-		$b->addCrumb('foo', 'bar');
-		$b->addCrumb('foo', 'bar');
-		$b->addCrumb('foo', 'bar');
+		$b = new Breadcrumbs($crumbs);
 
-		Assert::equals(3, $b->count());
+		Assert::equals(count($crumbs), $b->count());
 	}
 
+	/**
+	 * Tests whether `Breadcrumbs::isEmpty()` works correctly.
+	 */
 	public function testIsEmpty()
 	{
 		$b = new Breadcrumbs();
@@ -196,7 +370,9 @@ class BreadcrumbsTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * @dataProvider crumbProvider
+	 * Tests whether `Breadcrumbs::removeAll()` works correctly.
+	 *
+	 * @dataProvider crumbsProvider
 	 */
 	public function testRemoveAll($crumbs)
 	{
